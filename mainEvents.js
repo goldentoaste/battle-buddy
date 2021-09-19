2984e61c420684e7e9deeea0da6a74366ce80cf5
Goldentoaste <rayg2375@gmail.com>
2021-09-18 20:33:24 -0700

    server testing done

--- mainEvents.js
+++ mainEvents.js
@@ -7,17 +7,32 @@ const fetch = require('node-fetch')
 
 
 
-child = execFile('./py/timer.exe')
+child = spawn('./py/parse.exe')
 
 
-
+// child.stdout.on('data', (data) => {
+//     console.log(data.toString())
+// })
 async function fetchData() {
-    const res = await fetch("http://localhost:8080/");
+    console.log('starting')
+    const res = await fetch("http://localhost:8080/",);
+    console.log('ending')
     const data = await res.text();
     console.log(data);
     return res;
 }
 
-child.on('spawn', () => {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+child.on('spawn', async () => {
+    console.log('before sleep')
+    await sleep(2000)
+    console.log('after sleep')
     fetchData();
 })
+
+child.on('SIGINT', () => { child.exit() })
+child.on('SIGTERM', () => { child.exit() })
